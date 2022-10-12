3b6c58755ecc2f25e45ea356b75386dfc2da28fc
agamjotsingh18 <70067726+agamjotsingh18@users.noreply.github.com>
2022-10-12 23:18:46 +0530

    Update Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,22 @@
 import React from "react";
+import SignIn from "./SignIn";
 
 const Home = ({ signOut, user }) => {
   console.log(user);
+
   return (
     <div className="wrapper">
-      <h1 className="main-heading">Welcome to TweepsBook👋, {user.phoneNumber}</h1>
-      <button className="main-button" id="signOut" onClick={signOut}>
+      <h1 className="main-heading">Welcome to TweepsBook👋 {user.phoneNumber}</h1>
+      { 
+      user?
+        <button className="main-button" id="signOut" onClick={signOut}>
         Sign Out
       </button>
+      :
+      <button className="main-button" onClick={event =>  window.location.href='/signIn'}>
+        Sign In
+      </button>
+}
     </div>
   );
 };
